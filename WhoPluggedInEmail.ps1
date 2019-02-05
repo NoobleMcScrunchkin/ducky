ef89dc89bd98881d059fb5ccf3f90b9413df46aa
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-05 10:55:10 +0000

    Update WhoPluggedInEmail.ps1

--- WhoPluggedInEmail.ps1
+++ WhoPluggedInEmail.ps1
@@ -1,15 +1,17 @@
-$email = "psnerdattack@outlook.com" 
-$pass = "gSCR7MjO8H9w"
-$smtpServer = "smtp-mail.outlook.com" 
-$msg = new-object Net.Mail.MailMessage 
-$smtp = new-object Net.Mail.SmtpClient($smtpServer, 587) 
-$smtp.EnableSsl = $true 
-$msg.From = "$email"
-$msg.To.Add("11182@elizabethcollege.gg") 
-$msg.BodyEncoding = [system.Text.Encoding]::Unicode 
-$msg.SubjectEncoding = [system.Text.Encoding]::Unicode 
-$msg.IsBodyHTML = $true  
-$msg.Subject = $env:UserName + " plugged in a bad usb" 
-$msg.Body = "<h2>User " + $env:UserName + " plugged in a bad usb!</h2>"  
-$smtp.Credentials = New-Object System.Net.NetworkCredential("$email", "$pass"); 
-$smtp.Send($msg)
+$smtpCred = New-Object System.Net.NetworkCredential("psnerdattack@outlook.com", "gSCR7MjO8H9w"); 
+$ToAdress = '11182@elizabethcollege.gg'
+$FromAddress = 'psnerdattack@outlook.com'
+$SmtpServer = 'smtp-mail.outlook.com'
+$SmtpPort = '587'
+
+$mailparam = @{
+	To = $ToAdress
+	From = $FromAddress
+	Subject = $env:UserName + " plugged in a bad usb"
+	Body = "<h2>User " + $env:UserName + " plugged in a bad usb!</h2>"  
+	SmtpServer = $SmtpServer
+	Port = $SmtpPort
+	Credential = $smtpCred
+}
+
+Send-MailMessage @mailparam -UseSsl
