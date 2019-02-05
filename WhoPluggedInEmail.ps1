d254782bc4b4d8c869c407b0530d80ae29b65c81
NoobleMcScrunchkin <kieranaslett@gmail.com>
2019-02-05 11:05:11 +0000

    adg

--- WhoPluggedInEmail.ps1
+++ WhoPluggedInEmail.ps1
@@ -1,4 +1,5 @@
-$smtpCred = New-Object System.Net.NetworkCredential("psnerdattack@outlook.com", "gSCR7MjO8H9w"); 
+$secpasswd = ConvertTo-SecureString "gSCR7MjO8H9w" -AsPlainText -Force
+$smtpCred = (New-Object System.Management.Automation.PSCredential("psnerdattack@outlook.com", $secpasswd))
 $ToAdress = '11182@elizabethcollege.gg'
 $FromAddress = 'psnerdattack@outlook.com'
 $SmtpServer = 'smtp-mail.outlook.com'
