5d59932a5697ee78caf7764b650103b754227595
Nooble <kieranaslett@gmail.com>
2019-02-06 20:28:36 +0000

    sddg

--- wuowiloveupdates/wuowiloveupdates.ino
+++ wuowiloveupdates/wuowiloveupdates.ino
@@ -7,7 +7,7 @@ void setup() {
 void loop() {
  DigiKeyboard.sendKeyStroke(0);
  DigiKeyboard.delay(200);
- DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
+ DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
  DigiKeyboard.delay(500);
  DigiKeyboard.print("iexplore -k http://fakeupdate.net/win10u/");
  DigiKeyboard.delay(200);
